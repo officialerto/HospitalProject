ae6528836ebf8491718f831f94178921521ac2cb
Ertugrul <57365047+officialerto@users.noreply.github.com>
2024-06-21 03:07:22 +0300

    HospitalProjct
    Bu proje, hastane randevu sistemi için ASP.NET MVC üzerinde ADO.NET kullanılarak EF-DB First yaklaşımıyla doktorlar, hastalar ve randevuları yönetmek amacıyla oluşturulmuş bir CRUD uygulamasıdır.
    Basit bir proje. Tamamen pratik ve hatırlamak için yapıldı.

---
+++ MovieReviewProject/MovieReviewProject/Controllers/AppointmentsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MovieReviewProject.Models;
+
+namespace MovieReviewProject.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        private HospitalDBEntities db = new HospitalDBEntities();
+
+        // GET: Appointments
+        public ActionResult Index()
+        {
+            var appointments = db.Appointments.Include(a => a.Doctor).Include(a => a.Patient);
+            return View(appointments.ToList());
+        }
+
+        // GET: Appointments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointment appointment = db.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointment);
+        }
+
+        // GET: Appointments/Create
+        public ActionResult Create()
+        {
+            ViewBag.doctor_id = new SelectList(db.Doctors, "doctor_id", "name");
+            ViewBag.patient_id = new SelectList(db.Patients, "patient_id", "name");
+            return View();
+        }
+
+        // POST: Appointments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,patient_id,doctor_id,appointment_date,status")] Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Appointments.Add(appointment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.doctor_id = new SelectList(db.Doctors, "doctor_id", "name", appointment.doctor_id);
+            ViewBag.patient_id = new SelectList(db.Patients, "patient_id", "name", appointment.patient_id);
+            return View(appointment);
+        }
+
+        // GET: Appointments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointment appointment = db.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.doctor_id = new SelectList(db.Doctors, "doctor_id", "name", appointment.doctor_id);
+            ViewBag.patient_id = new SelectList(db.Patients, "patient_id", "name", appointment.patient_id);
+            return View(appointment);
+        }
+
+        // POST: Appointments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,patient_id,doctor_id,appointment_date,status")] Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(appointment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.doctor_id = new SelectList(db.Doctors, "doctor_id", "name", appointment.doctor_id);
+            ViewBag.patient_id = new SelectList(db.Patients, "patient_id", "name", appointment.patient_id);
+            return View(appointment);
+        }
+
+        // GET: Appointments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointment appointment = db.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointment);
+        }
+
+        // POST: Appointments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Appointment appointment = db.Appointments.Find(id);
+            db.Appointments.Remove(appointment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
